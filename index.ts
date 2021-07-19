e776ba4e66587604e87ee68543dcfb5c5bce74b4
kmg31815 <kmg31815@gmail.com>
2021-07-19 17:48:28 +0800

    startWith()、takeUntil()、endWith()

--- index.ts
+++ index.ts
@@ -1,30 +1,14 @@
-import { concat, interval, range } from 'rxjs';
-import { take, tap } from 'rxjs/operators';
+import { interval, fromEvent } from 'rxjs';
+import { map, startWith, takeUntil, endWith } from 'rxjs/operators';
 
-const timer$ = interval(1000).pipe(
-  take(4),
-  tap(d => {
-    /**
-     *  tap() 可以用來
-     *    在不改變 Observable 的情況下
-     *    * 查看console
-     *    * 拋出錯誤
-     *    * 於隨機情況下得知一個 observable 何時完成
-     */
-    console.log(' >> ' + d);
-  })
-);
-const sequence$ = range(1, 10);
+const ticker$ = interval(5000).pipe(map(() => 'tick'));
 
-/**
- * concat()
- *  在第8版後移除，以 concatWith 取代
- *
- *  concat(a$, b$)與 a$.pipe(concatWith(b$))相同
- *  => a$ 被監聽了才會去監聽 b$
- */
-const result$ = concat(timer$, sequence$);
-const result2$ = concat(sequence$, timer$);
+const documentClicks$ = fromEvent(document, 'click');
 
-result$.subscribe(x => console.log(x));
-result2$.subscribe(y => console.log('result2 >>' + y));
+ticker$
+  .pipe(
+    startWith('interval started'),
+    takeUntil(documentClicks$), // 當監聽到 documentClicks$ 事件，endWith()
+    endWith('interval ended by click')
+  )
+  .subscribe(console.log);
