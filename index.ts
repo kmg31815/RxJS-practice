0155a68333f3a13fa5d584a813527bbcfa82bbf9
kmg31815 <kmg31815@gmail.com>
2021-07-19 15:29:21 +0800

    非同步測試

--- index.ts
+++ index.ts
@@ -1,17 +1,27 @@
 import { Observable } from 'rxjs';
 
 const source$ = new Observable(subscriber => {
-  console.log('stream ');
+  console.log('stream head');
   subscriber.next(1);
   subscriber.next(2);
   subscriber.next(3);
-  subscriber.next(4);
-  console.log('stream ');
+  setTimeout(() => {
+    subscriber.next(4);
+    subscriber.complete();
+    console.log('stream foot');
+  }, 100);
   subscriber.complete(console.log('subscriber complete'));
 });
 
+/**
+ * 非同步
+ */
 // 每當訂閱發生，便執行callback function內的程式碼
 source$.subscribe({
-  next: data => console.log(`Observable : ${data}`),
-  complete: () => console.log('complete')
+  next: data => console.log(`Observable1 : ${data}`),
+  complete: () => console.log('complete1')
+});
+source$.subscribe({
+  next: data => console.log(`Observable2 : ${data}`),
+  complete: () => console.log('complete2')
 });
