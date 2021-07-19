a06bc9577f9af16fdc2655008deab56a4e4a2827
kmg31815 <kmg31815@gmail.com>
2021-07-19 16:01:08 +0800

    ReplaySubject(N) 重播最近 N 次事件資料

--- index.ts
+++ index.ts
@@ -1,15 +1,14 @@
-import { BehaviorSubject } from 'rxjs';
+import { ReplaySubject } from 'rxjs';
 
 /**
- * BehaviorSubject
+ * ReplaySubject(N)
  */
-// 訂閱時會得到"最後一次"的事件資訊，因此，建立物件實體需要給予一個"初始值"，作為最近一次的事件值
-const source$ = new BehaviorSubject(0);
-source$.subscribe(data => console.log(`1 : ${data}`));
-source$.next(1);
-console.log('value >> ' + source$.value);
-source$.subscribe(data => console.log(`2 : ${data}`));
-source$.next(2);
-source$.complete();
-source$.subscribe(data => console.log(`3 : ${data}`));
-console.log('value >> ' + source$.value);
+// 每當物件實體被訂閱時會重播最近 N 次事件資料
+const source$ = new ReplaySubject(3);
+source$.subscribe(data => console.log(`第一次訂閱 : ${data}`));
+for (let i = 0; i < 5; ++i) {
+  source$.next(i);
+}
+source$.subscribe(data => console.log(`第二次訂閱 : ${data}`));
+source$.complete(); // 就算資料流發生錯誤或完成，每次訂閱一樣會收到最近N次事件資料
+source$.subscribe(data => console.log(`第三次訂閱 : ${data}`));
